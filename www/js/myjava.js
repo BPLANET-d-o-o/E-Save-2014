f3534d34e9036fab8da8a0ed6e58fa2723a4c216
Luka Elsner <luka.elsner@gmail.com>
2014-10-08 11:43:54 +0200

    device ready

--- www/js/myjava.js
+++ www/js/myjava.js
@@ -1,20 +1,22 @@
-function isAndroid(){
-	return navigator.userAgent.indexOf("Android") > 0;
-}
+document.addEventListener('deviceready', function () {
+	function isAndroid(){
+		return navigator.userAgent.indexOf("Android") > 0;
+	}
 
-function isiOS(){
-	return (navigator.userAgent.indexOf("iPhone") > 0 || navigator.userAgent.indexOf("iPad") > 0 || navigator.userAgent.indexOf("iPod") > 0); 
-}
+	function isiOS(){
+		return (navigator.userAgent.indexOf("iPhone") > 0 || navigator.userAgent.indexOf("iPad") > 0 || navigator.userAgent.indexOf("iPod") > 0); 
+	}
 
-function isBlackBerry(){
-	return navigator.userAgent.indexOf("BlackBerry") > 0;
-}
+	function isBlackBerry(){
+		return navigator.userAgent.indexOf("BlackBerry") > 0;
+	}
 
-function showAlert(title, message) {
-	navigator.notification.alert(
-		message,
-		null,
-		title,
-		'V REDU'
-	);
-}
+	function showAlert(title, message) {
+		navigator.notification.alert(
+			message,
+			null,
+			title,
+			'V REDU'
+		);
+	}
+}, false);
