bd7405045041e0d384f3a9922d2ec748fbe9ef80
Luka Elsner <luka.elsner@gmail.com>
2014-10-08 13:10:08 +0200

    v2

--- www/js/myjava.js
+++ www/js/myjava.js
@@ -1,22 +1,8 @@
-document.addEventListener('deviceready', function () {
-	function isAndroid(){
-		return navigator.userAgent.indexOf("Android") > 0;
-	}
-
-	function isiOS(){
-		return (navigator.userAgent.indexOf("iPhone") > 0 || navigator.userAgent.indexOf("iPad") > 0 || navigator.userAgent.indexOf("iPod") > 0); 
-	}
-
-	function isBlackBerry(){
-		return navigator.userAgent.indexOf("BlackBerry") > 0;
-	}
-
-	function showAlert(title, message) {
-		navigator.notification.alert(
-			message,
-			null,
-			title,
-			'V REDU'
-		);
-	}
-}, false);
+function showAlert(title, message) {
+	navigator.notification.alert(
+		message,
+		null,
+		title,
+		'V REDU'
+	);
+}
